ecc16cfc108193564e2f7a7e81706a18a6b8e957
dra072 <akhtardanish72@gmail.com>
2026-09-22 03:38:14 +0530

    update live chat11

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,13 +31,13 @@ function App() {
 
 useEffect(() => {
     fetchWishes();
-    const interval = setInterval(() => {
-        fetchWishes();
-    }, 2000);
+    // const interval = setInterval(() => {
+    //     fetchWishes();
+    // }, 2000);
 
-    return () => {
-        clearInterval(interval);
-    };
+    // return () => {
+    //     clearInterval(interval);
+    // };
 }, []);
 
   const handleSubmit = async (e) => {
